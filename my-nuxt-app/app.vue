8cede43b1b44c31d8527dd618e72bae97a01b079
Monika Bohn <monika.bohn@eduhouse.fi>
2025-06-23 16:02:24 +0200

    Initial commit

---
+++ my-nuxt-app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtPage/>
+  </div>
+</template>
+
+<script lang="ts">
+import '@nordhealth/css'
+import '@nordhealth/components'
+</script>
---
+++ my-nuxt-app/stores/types/types.ts
@@ -0,0 +1,9 @@
+export interface FormData {
+    email: string
+    password: string,
+    productUpdates: boolean
+}
+export interface FormErrors {
+    email: string
+    password: string
+}
